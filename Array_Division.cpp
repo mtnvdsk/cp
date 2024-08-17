c58cff94c9b367039d9489e457a5ab12ffd48c19
mtnvdsk <mtnvdsk@gmail.com>
2024-08-17 13:49:50 +0530

    updated

---
+++ Array_Division.cpp
@@ -0,0 +1,67 @@
+//BY: tardum
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int lli;
+//---- Debugger ---- //
+#define debarr(a,n) cout<<#a<<" : ";for(int i=0;i<n;i++) cerr<<a[i]<<" "; cerr<<endl;
+#define debmat(mat,row,col) cout<<#mat<<" :\n";for(int i=0;i<row;i++) {for(int j=0;j<col;j++) cerr<<mat[i][j]<<" ";cerr<<endl;}
+#define pr(...) dbs(#__VA_ARGS__, __VA_ARGS__)
+template <class S, class T>ostream& operator <<(ostream& os, const pair<S, T>& p) {return os << "(" << p.first << ", " << p.second << ")";}
+template <class T>ostream& operator <<(ostream& os, const vector<T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class T>ostream& operator <<(ostream& os, const unordered_set<T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class S, class T>ostream& operator <<(ostream& os, const unordered_map<S, T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class T>ostream& operator <<(ostream& os, const set<T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class T>ostream& operator <<(ostream& os, const multiset<T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class S, class T>ostream& operator <<(ostream& os, const map<S, T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class T> void dbs(string str, T t) {cerr << str << " : " << t << "\n";}
+template <class T, class... S> void dbs(string str, T t, S... s) {int idx = str.find(','); cerr << str.substr(0, idx) << " : " << t << ","; dbs(str.substr(idx + 1), s...);}
+template <class T> void prc(T a, T b) {cerr << "["; for (T i = a; i != b; ++i) {if (i != a) cerr << ", "; cerr << *i;} cerr << "]\n";}
+lli binpow(lli b,lli p,lli mod){lli ans=1;b%=mod;for(;p;p>>=1){if(p&1)ans=ans*b%mod;b=b*b%mod;}return ans;}
+//----------------- //
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define per(i, a, b) for (int i = a; i > b; i--)
+#define all(vec) vec.begin(), vec.end()
+#define endl '\n'
+typedef long long ll;
+const int N = 1e3+5;
+
+signed main(){
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int t=1;
+    //cin>>t;
+    while(t--){
+        ll n,k;
+        cin>>n>>k;
+        vector<ll> a(n);
+
+        rep(i, 0, n) cin >> a[i];
+        
+        ll l = *max_element(a.begin(), a.end()); 
+        ll r = accumulate(a.begin(), a.end(), 0LL);
+        ll ans = 0;
+        while(l<=r){
+            ll mid=l+(r-l)/2.0;
+
+            ll s=0;
+            ll c=1;
+            rep(i,0,n){
+                if(s+a[i]>mid){
+                    c++;
+                    s=a[i];
+                }
+                else s+=a[i];
+            }
+            if(c<=k){
+                ans=mid;
+                r=mid-1;
+            }
+            else l=mid+1;
+        }
+        cout<<ans<<endl;
+    }
+    return 0;
+}
